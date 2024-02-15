6da805ab8457cb297624bba2bb519b84fbbf9b3a
Satgoltratec <116006255+Satgoltratec@users.noreply.github.com>
2024-02-15 18:48:37 +0100

    cambios diseño

--- components/Container.tsx
+++ components/Container.tsx
@@ -11,6 +11,7 @@ import {
   View,
   TextInput,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 import {data, House} from './data';
 
@@ -20,118 +21,170 @@ function Container() {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-        }}>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
         <View
           style={{
-            paddingHorizontal: 12,
-            flexDirection: 'row',
-            backgroundColor: 'lightgrey',
-            height: 50,
-            alignItems: 'center',
+            flex: 1,
           }}>
-          {/* Barra de búsqueda */}
-          <View style={{flexGrow: 1}}>
-            <TextInput
-              style={{
-                height: 35,
-                marginRight: 12,
-                borderWidth: 1,
-
-                padding: 5,
-                borderRadius: 10,
-              }}
-              placeholder="Busqueda"
-            />
-          </View>
-
-          <View style={{backgroundColor: 'tomato', width: 80}}>
-            <Button title="FILTRO" />
-          </View>
-        </View>
-        <View
-          style={{backgroundColor: 'tomato', height: 50, flexDirection: 'row'}}>
-          <ScrollView horizontal={true}>
-            <View style={styles.items}>
-              <View style={styles.item}>
-                <Text>ScrollItem 1</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 2</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 3</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 4</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 5</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 6</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 7</Text>
-              </View>
-              <View style={styles.item}>
-                <Text>ScrollItem 8</Text>
-              </View>
-            </View>
-          </ScrollView>
-        </View>
-
-        <ScrollView style={styles.scrollViewContent}>
-          {data.map(house => (
-            <View key={house.id} style={{padding: 10}}>
-              <Image
-                source={{uri: house.imageUrl}}
+          <View
+            style={{
+              paddingHorizontal: 12,
+              flexDirection: 'row',
+              backgroundColor: '#fefefe',
+              height: 50,
+              alignItems: 'center',
+            }}>
+            {/* Barra de búsqueda */}
+            <View style={{flexGrow: 1}}>
+              <TextInput
                 style={{
-                  height: 250,
-                  borderRadius: 20,
+                  height: 35,
+                  marginRight: 12,
+                  borderWidth: 1,
+                  borderColor: '#e9e9e9',
+                  shadowColor: '#f6f6f6',
+                  shadowOffset: {width: 0, height: 2},
+                  shadowOpacity: 0.1,
+                  shadowRadius: 4,
+                  padding: 5,
+                  borderRadius: 50,
                 }}
+                placeholder="¿Dónde quieres ir?"
               />
-              <View style={{padding: 7}}>
-                <Text style={{fontWeight: 'bold'}}>
-                  {house.province}, {house.country}
-                </Text>
-                <Text>{house.owner}</Text>
-
-                <Text>{house.pricePerNight} € por noche</Text>
-              </View>
             </View>
-          ))}
-        </ScrollView>
 
-        <Pressable style={styles.buttonMap} onPress={onPressFunction}>
-          <Text style={styles.textButton}>BOTON</Text>
-        </Pressable>
+            <View>
+              <Pressable
+                style={{
+                  width: 45,
+                  height: 45,
+                  borderRadius: 50, // Esto hace que el botón sea circular
+                  backgroundColor: '#ffffff',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderColor: 'grey',
+                  borderWidth: 1,
+                }}>
+                <Text>x</Text>
+                <Text>Filtro</Text>
+              </Pressable>
+              {/* <Button title="FILTRO" /> */}
+            </View>
+          </View>
+          <View>
+            <ScrollView
+              showsHorizontalScrollIndicator={false}
+              horizontal={true}
+              style={{
+                backgroundColor: '#fcfcfc',
+                height: 50,
+                flexDirection: 'row',
+              }}>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Piscinas increíbles</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Castillos</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Islas</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Casas domo</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Desiertos</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>En el campo</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>A pie de playa</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Cabañas</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Artico</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Cuevas</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Camping</Text>
+              </View>
+              <View style={styles.item}>
+                <Text>x</Text>
+                <Text>Diseño</Text>
+              </View>
+            </ScrollView>
+          </View>
 
-        <View style={styles.bottomMenu}>
-          {/* Barra Inferior */}
+          <ScrollView style={styles.scrollViewContent}>
+            {data.map(house => (
+              <View key={house.id} style={{padding: 10}}>
+                <Image
+                  source={{uri: house.imageUrl}}
+                  style={{
+                    height: 250,
+                    borderRadius: 20,
+                  }}
+                />
+                <View style={{padding: 7}}>
+                  <Text style={{fontWeight: 'bold'}}>
+                    {house.province}, {house.country}
+                  </Text>
+                  <Text>{house.owner}</Text>
 
-          <View style={styles.itemBotton}>
-            <Text>Item 1</Text>
-            <Text>x</Text>
-          </View>
-          <View style={styles.itemBotton}>
-            <Text>Item 1</Text>
-          </View>
-          <View style={styles.itemBotton}>
-            <Text>Item 1</Text>
-          </View>
-          <View style={styles.itemBotton}>
-            <Text>Item 1</Text>
-          </View>
-          <View style={styles.itemBotton}>
-            <Text>Item 1</Text>
+                  <Text>{house.pricePerNight} € por noche</Text>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+
+          <Pressable style={styles.buttonMap} onPress={onPressFunction}>
+            <Text style={styles.textButton}>Mapa</Text>
+          </Pressable>
+
+          <View style={styles.bottomMenu}>
+            {/* Barra Inferior */}
+
+            <View style={styles.itemBotton}>
+              <Text>x</Text>
+              <Text>Explora</Text>
+            </View>
+            <View style={styles.itemBotton}>
+              <Text>x</Text>
+              <Text>Favoritos</Text>
+            </View>
+            <View style={styles.itemBotton}>
+              <Text>x</Text>
+              <Text>Viajes</Text>
+            </View>
+            <View style={styles.itemBotton}>
+              <Text>x</Text>
+              <Text>Mensajes</Text>
+            </View>
+            <View style={styles.itemBotton}>
+              <Text>x</Text>
+              <Text>Iniciar sesión</Text>
+            </View>
           </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
@@ -141,35 +194,37 @@ const styles = StyleSheet.create({
   },
   search: {
     flexDirection: 'row',
-    backgroundColor: 'lightgrey',
+    backgroundColor: '#f7f7f7',
     height: 50,
   },
   items: {
     flexDirection: 'row', // Items horizontalmente
     height: 50,
+    backgroundColor: 'tomato',
+    borderBottomColor: '#f9f9f9',
   },
   itemsBotton: {
     flexDirection: 'row', // Items horizontalmente
     height: 50,
   },
   item: {
-    borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
     flexGrow: 1,
+    paddingHorizontal: 10,
   },
   itemBotton: {
-    borderWidth: 1,
+    borderTopColor: '#d7d7d7',
     justifyContent: 'center',
     alignItems: 'center',
     flexGrow: 1,
   },
 
   scrollViewContent: {
-    backgroundColor: 'lightblue',
+    backgroundColor: '#ffffff',
   },
   bottomMenu: {
-    backgroundColor: 'lightgreen',
+    backgroundColor: '#ffffff',
     height: 50,
     flexDirection: 'row',
   },
